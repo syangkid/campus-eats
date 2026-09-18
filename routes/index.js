8334751750e17aaba5f9781b9e2f2c88eb041c2e
syangkid <yangkisonam433@gmail.com>
2026-09-18 11:51:57 +0600

    Unit IV Lab 4: Order model, full CRUD, validation, aggregation, and transactions

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const homeController = require('../controllers/homeController');
 const aboutController = require('../controllers/aboutController');
 const menuController = require('../controllers/menuController');
 const orderController = require('../controllers/orderController');
-
 router.post('/orders', orderController.createOrder);
+router.get('/orders/:id', orderController.getOrder);
 router.get('/restaurants/:id/menu', menuController.getMenuByRestaurant);
 router.get('/', homeController.getHome);
 router.get('/about', aboutController.getAbout);
-
+router.post('/orders/:id/update', orderController.updateOrder);
+router.post('/orders/:id/cancel', orderController.cancelOrder);
 module.exports = router; 
